0f07377cb226b0eba8439fbca0ed45fc26ecd9de
yousuf mohammed <yousuf mohammed>
2016-09-03 22:43:53 -0700

    Added authenticated user name

--- OwinKatana2/TestOwinMiddleWare.cs
+++ OwinKatana2/TestOwinMiddleWare.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Web;
 using Microsoft.Owin;
@@ -9,7 +10,7 @@ namespace OwinKatana2
 {
     public class TestOwinMiddleWare
     {
-        private Func<IDictionary<string, object>, Task> m_next;
+        private readonly Func<IDictionary<string, object>, Task> m_next;
 
         public TestOwinMiddleWare(Func<IDictionary<string, object>, Task>  next)
         {
@@ -19,6 +20,8 @@ namespace OwinKatana2
         public async Task Invoke(IDictionary<string, object> env)
         {
             var context = new OwinContext(env);
+            //authentication code comes here
+            context.Request.User = new GenericPrincipal(new GenericIdentity("yousuf"),new string[] {});            
             Helper.Write("TestOwinMiddleWare", context.Request.User);
             await m_next(env);
         }
